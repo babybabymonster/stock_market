abcaf4b6554e8bd74e1334951e830f6c9b5d79f5
Ekaterina Lebedeva <lebekate@gmail.com>
2017-09-04 21:36:04 +1000

    Edit makeOrders

--- Sources/Order.hs
+++ Sources/Order.hs
@@ -2,8 +2,11 @@ module Order where
 
 import Types
 
+-- Change this implementation to your own non-trivial trading strategy.
+-- Do not modify the type signature of the function.
+--
 makeOrders :: Portfolio -> [StockHistory] -> [Order]
 makeOrders (cash, _) history = 
     case history of
         []   -> []
-        (s,p):_ -> [Order s (floor (cash / head p)) ] 
+        (s,p):_ -> [Order s (floor (cash / 10 * head p))] 
